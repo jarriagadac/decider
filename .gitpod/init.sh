7792fb57ad200b33bde19977ff320b15c018284b
Juan Arriagada <7169909+jarriagadac@users.noreply.github.com>
2023-03-03 22:50:33 -0300

    🔨 gitpod cfg inicial

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -2,11 +2,9 @@
 
 echo INIT
 
-
-psql -U gitpod -c "SELECT 1 FROM pg_database WHERE datname = 'decider'" | grep -q 1 || psql -U gitpod -c "CREATE DATABASE decider"
-psql -U gitpod -c "ALTER USER gitpod WITH PASSWORD 'GitPod00!#';"
+psql -U gitpod -c "ALTER USER gitpod WITH PASSWORD '!soCHisi2023!';"
 
 cd /workspace/decider
 source venv/bin/activate
-# python initial_setup.py
 python -m app.utils.db.actions.full_build [--config CONF]: /jsons/source → DB
+python initial_setup.py
